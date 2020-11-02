e04b4647218590e434311b0bcda56353556dd553
LCDS97 <lucas.cds1997@gmail.com>
2020-11-01 23:42:49 -0300

    Rich Notifications Chrome
    Abilitando notificações quando excede os gastos do valor estabelecido

--- budgetManager/js/popup.js
+++ budgetManager/js/popup.js
@@ -5,8 +5,8 @@ $(function(){
         $('#limit').text(budget.limit);
     })
 
-    $('#spendAmount').click(function (){
-        chrome.storage.sync.get('total', function(budget){
+    $('#spendAmount').on("click",function (){
+        chrome.storage.sync.get(['total','limit'], function(budget){
             var newTotal = 0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -17,7 +17,18 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){
+                if (amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: 'basic',
+                        iconUrl: 'img/icon48.png',
+                        title: 'Limite excedido!',
+                        message: 'Parece que você ja chegou no seu limite'
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                    chrome.notifications.clear('limitNotif'); 
+                }
+            });
 
             $('#total').text(newTotal);
             $('#amount').val('');
